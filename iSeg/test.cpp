7516e753d6c99b6e87e88d3c236a6f3fddc3f0df
Bryn Lloyd <lloyd@itis.ethz.ch>
2019-01-16 10:05:24 +0100

    added panning via mouse and added callback for gestures

--- iSeg/test.cpp
+++ iSeg/test.cpp
@@ -1,5 +1,6 @@
 #include <QtGui>
  
+#include <iostream>
 
 class CustomGraphicsView : public QGraphicsView
 {
@@ -8,9 +9,22 @@ public:
 
 protected:
     virtual void wheelEvent(QWheelEvent *event);
+	virtual void mouseMoveEvent(QMouseEvent *event);
+	virtual void mousePressEvent(QMouseEvent *event);
+	virtual void mouseReleaseEvent(QMouseEvent *event);
+	virtual bool event(QEvent *event)
+	{
+		if (event->type() == QEvent::Gesture)
+			return gestureEvent(static_cast<QGestureEvent*>(event));
+		return QWidget::event(event);
+	}
+	bool gestureEvent(QGestureEvent *event);
 
 private:
     const double scaleFactor = 1.2;
+
+	bool _pan = false;
+	int _panStartX,_panStartY;
 };
 
 CustomGraphicsView::CustomGraphicsView(QGraphicsScene* scene, QWidget *parent) 
@@ -29,13 +43,65 @@ void CustomGraphicsView::wheelEvent(QWheelEvent *event)
     }
 }
 
+void CustomGraphicsView::mousePressEvent(QMouseEvent *event)
+{
+	if (event->button() == Qt::RightButton)
+	{
+		_pan = true;
+		_panStartX = event->x();
+		_panStartY = event->y();
+		setCursor(Qt::ClosedHandCursor);
+		event->accept();
+		return;
+	}
+	event->ignore(); //?
+}
+
+void CustomGraphicsView::mouseReleaseEvent(QMouseEvent *event)
+{
+	if (event->button() == Qt::RightButton)
+	{
+		_pan = false;
+		setCursor(Qt::ArrowCursor);
+		event->accept();
+		return;
+	}
+	event->ignore(); //?
+}
+
+void CustomGraphicsView::mouseMoveEvent(QMouseEvent *event)
+{
+	if (_pan)
+	{
+		horizontalScrollBar()->setValue(horizontalScrollBar()->value() - (event->x() - _panStartX));
+		verticalScrollBar()->setValue(verticalScrollBar()->value() - (event->y() - _panStartY));
+		_panStartX = event->x();
+		_panStartY = event->y();
+		event->accept();
+		return;
+	}
+	event->ignore(); //?
+}
+
+bool CustomGraphicsView::gestureEvent(QGestureEvent *event)
+{
+	if (QGesture *swipe = event->gesture(Qt::SwipeGesture))
+		std::cerr << "swipe\n"; //swipeTriggered(static_cast<QSwipeGesture *>(swipe));
+	else if (QGesture *pan = event->gesture(Qt::PanGesture))
+		std::cerr << "pan\n"; //panTriggered(static_cast<QPanGesture *>(pan));
+	if (QGesture *pinch = event->gesture(Qt::PinchGesture))
+		std::cerr << "pinch\n"; //pinchTriggered(static_cast<QPinchGesture *>(pinch));
+	return true;
+}
+
 int main( int argc, char **argv )
 {
 	QApplication app(argc, argv);
 	CustomGraphicsView view(new QGraphicsScene );
 	view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-	view.scene()->addPixmap(QPixmap("/Users/lloyd/bryn-lloyd.png"));
+	//view.scene()->addPixmap(QPixmap("/Users/lloyd/bryn-lloyd.png"));
+	view.scene()->addPixmap(QPixmap("E:/VKF/0606.png"));
 	view.show();
 	return app.exec(); 
 }
